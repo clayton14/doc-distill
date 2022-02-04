cf7684e1c2956b8a6a2e6e38b89d98799c0426ba
clayton14 <claytoneasley77@gmail.com>
2022-02-04 14:01:28 -0500

    extracts text from pdf

--- reader.py
+++ reader.py
@@ -4,26 +4,30 @@ Reads pdf file and extracts info
 import os
 import sys
 from PyPDF2 import PdfFileReader
+"""
+Reads PDF file and extracts info
+"""
 
 
 class Reader():
     def __init__(self, file=None,):
         self.file = file
-        self.all_text = None
-        self.page_text = []
+        self.all_text = ""
+        self.text_on_page = []
         self.images = []
 
     def read(self):
+        """
+        returns text found on document
+        """
         try:
             with open(self.file, 'rb') as pdf_file:
+                print(f"[READING] {self.file}")
                 doc = PdfFileReader(pdf_file)
-
-                # if("not scaned"):
-                #     # PyPdf
-                #     pass
-                # else:
-                #     # use ocr
-                #     pass
+                for i in range(doc.numPages):
+                    page = doc.getPage(i)
+                    self.text_on_page.append(page.extractText())
+                    self.all_text += page.extractText()
         except FileNotFoundError as err:
             print("[ERROR] file not found")
 
