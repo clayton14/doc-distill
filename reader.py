1eef03c429159e6607f83a401e1f180c851c5de3
clayton14 <claytoneasley77@gmail.com>
2022-02-04 20:14:53 -0500

    summarize method frequency table

--- reader.py
+++ reader.py
@@ -1,13 +1,13 @@
 """
-Reads pdf file and extracts info
+Reads pdf file and extracts info and summorizes it
 """
 import os
 import sys
+import string
 import pandas
 from PyPDF2 import PdfFileReader
-"""
-Reads PDF file and extracts info
-"""
+from nltk import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
 
 
 class Reader():
@@ -16,9 +16,12 @@ class Reader():
         self.all_text = ""
         self.title = ""
         self.text_on_page = []
-        self.images = []
+        self.read()
 
     def read(self):
+        """
+        Reads pdf file and extracts text
+        """
         try:
             with open(self.file, 'rb') as pdf_file:
                 print(f"[READING] {self.file}")
@@ -33,5 +36,24 @@ class Reader():
     def get_table(self, page_num):
         pass
 
-    def summarize(self, text):
+    def extract_sentences(self):
+        return sent_tokenize(self.all_text)
+
+    def find_email(self):
         pass
+
+    def find_all_links(self):
+        pass
+
+    def summarize(self):
+        words = word_tokenize(self.all_text)
+        stop_words = set(stopwords.words("english"))
+        freqTable = {}
+        for word in words:
+            word = word.lower()
+            if(word in stop_words):
+                continue
+            if(word in freqTable):
+                freqTable[word] += 1
+            else:
+                freqTable[word] = 1
