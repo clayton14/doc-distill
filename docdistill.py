0a40c67d5a4c199362055ddafa405656f7f4e40e
clayton14 <claytoneasley77@gmail.com>
2022-02-07 08:30:40 -0500

    docdistill will hold all the summarizors

--- docdistill.py
+++ docdistill.py
@@ -1,23 +1,41 @@
 # Contains the main loop for the framwork
 #
-import pyfiglet
-import random
+
+class DocDistill():
+    def __init__(self, text=None):
+        self.text = text
 
 
-def main():
-    while True:
-        start = input(">>>")
-        if(start == "help"):
-            print(
-                "options:\nweb\tinput the url to a website to extract text\nfind\tsearch for PDF in current dir")
-        elif(start == "web"):
-            web_input = input("[URL]>>>")
-        elif(start == "find"):
-            print("looking for PDF docuiment in current dir")
+    def summarize(self, size=3):
+        self._clean()
+        summary = ""
+        stop_words = stopwords.words("english")
+        words = word_tokenize(self.all_text.lower())
+        sentences = sent_tokenize(self.all_text)
+        sent_score = {}
+        freq_table = {}
+        # building frequency table
+        for word in words:
+            if(word.lower() not in stop_words):
+                if(word.lower() not in string.punctuation):
+                    if(word not in freq_table.keys()):
+                        freq_table[word] = 1
+                    else:
+                        freq_table[word] += 1
+        # devide by max
+        max_freq = max(freq_table.values())
+        for word in freq_table.keys():
+            freq_table[word] = freq_table[word]/max_freq
+        # ranking each sentence
+        for sent in sentences:
+            for word in sent:
+                if (word.lower() in freq_table.keys()):
+                    if (sent not in sent_score.keys()):
+                        sent_score[sent] = freq_table[word.lower()]
+                    else:
+                        sent_score[sent] += freq_table[word.lower()]
 
-
-if __name__ == "__main__":
-    art = ["smisome1", "5lineoblique", "block", "larry3d"]
-    print(pyfiglet.figlet_format("DocDistill", font=random.choice(art)))
-    print("type help for list of options")
-    main()
+        for i in range(size):
+            largest = nlargest(size, sent_score, key=sent_score.get)
+            summary = ''.join(largest)
+        return summary.replace("\n", "")
